c3445854779b29512b1f93e4ecb9543f134a46d2
vivek ranjnan <vivekranjan2510@gmail.com>
2024-04-20 11:46:43 +0530

    Customer repo added

---
+++ src/main/java/com/banking/customerms/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.banking.customerms.repository;
+
+import com.banking.customerms.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+}
